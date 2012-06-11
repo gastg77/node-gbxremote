5ceff4cab09130802961de9bdf4060fe4805f6ca
Kristjan Broder Lund <kristjan.1234@gmail.com>
2012-06-11 20:21:23 +0000

    Remember ovebuffered data

--- example/client.js
+++ example/client.js
@@ -6,7 +6,8 @@ var client = xmlrpc.createClient(5000, '192.168.1.3', function(err) {
 		console.log("error", err);
 	} else {
 		console.log('connected');
-		//return;
+		
+		
 		// Sends a method call to the XML-RPC server
 		client.methodCall('GetVersion', null, function(error, value) {
 			console.log('response');
@@ -15,7 +16,43 @@ var client = xmlrpc.createClient(5000, '192.168.1.3', function(err) {
 			} else {
 				// Results of the method response
 				console.log('Method response for "GetVersion": ', value);
+				
+				setTimeout(speedTest, 2000);
 			}
 		});
+		
+		
+		
 	}
 });
+
+
+var speedTest = function() {
+	var commands = [
+		'GetVersion',
+		'GetCurrentCallVote',
+		'GetCallVoteTimeOut',
+		'GetCallVoteRatio',
+		'GetCallVoteRatios',
+		'GetChatLines',
+		'GetManialinkPageAnswers',
+		'GetServerCoppers',
+		'GetSystemInfo',
+		'GetServerName',
+		'GetServerComment'
+	];
+	
+	console.log('Speedtest!!!');
+	
+	var answers = {};
+	commands.forEach(function(method) {
+		client.methodCall(method, null, function(error, value) {
+			answers[method] = value;
+			console.log(method, value);
+		});
+	});
+	
+	setTimeout(function() {
+		console.log("all answers:", answers);
+	}, 2000);
+};
